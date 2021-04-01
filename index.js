55a954d40c80f2fdb8abf6cea38363f30ad7cb64
arynthernium <arynthernium@gmail.com>
2021-04-01 17:33:48 -0400

    working request

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const fs = require('fs');
+const jp = require('jsonpath');
 
 app.get('/', function(req, res) {
     res.render('index.ejs');
@@ -11,12 +12,15 @@ app.get('/', function(req, res) {
 
 io.sockets.on('connection', function(socket) {
 
+	io.emit('prompt');
+	console.log('Client connected')
+
     socket.on('password', function(password) {
+
         if (password == process.env.password) {
 			console.log('Client logged in as: '+ socket.id);
 
-			var jsondata = `[{"id": "123","name": "jared"},{"id": "1234","name": "john"}]`
-			console.log(JSON.parse(jsondata));
+			var jsondata = fs.readFileSync('./json/table.json', 'utf8');
 
 			io.emit('login', jsondata);
 		} else {
@@ -25,8 +29,19 @@ io.sockets.on('connection', function(socket) {
 		}
     });
 
+	socket.on('request', function(requestid) {
+        console.log('Requested entry: '+ requestid);
+
+			var jsondata = JSON.parse(fs.readFileSync('./json/table.json', 'utf8'));
+			returnjson = jp.query(jsondata, `$..[?(@.id =='${requestid}')]`);
+			console.log(returnjson)
+			io.emit('returnEntry', returnjson);
+    });
+
 });
 
 const server = http.listen(8080, function() {
     console.log('listening on http://localhost:8080');
 });
+
+// 
